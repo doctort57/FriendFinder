ea7a0fe361ceacef24f8c372df942ee671939d98
David Thurman <dvdthurman@gmail.com>
2017-11-03 16:51:52 -0400

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Monica",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/t31.0-8/13116244_10107568960519320_7024834722285826624_o.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Simon",
+	"photo" : "http://www.gettyimages.com/event/archive-entertainment-on-wire-image-bruce-springsteen-143415129#portrait-of-american-rock-singer-and-songwriter-bruce-springsteen-in-picture-id2484015",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Rhea",
+	"photo" : "https://scontent-dfw1-1.xx.fbcdn.net/v/t1.0-9/13450242_1233540543375869_9163241940047961665_n.jpg?oh=bb6cd85e9090d902fa663ff864f440e5&oe=5821AB84",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
